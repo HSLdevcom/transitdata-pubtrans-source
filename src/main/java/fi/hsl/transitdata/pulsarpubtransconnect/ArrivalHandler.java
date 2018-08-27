59af3d0458e8b06c94a6e358b14a76be7e6ce057
Markus Paasovaara <markus.paasovaara@futurice.com>
2018-08-27 14:38:08 +0300

    Get proto classes from commons and use the base-structure

--- src/main/java/fi/hsl/transitdata/pulsarpubtransconnect/ArrivalHandler.java
+++ src/main/java/fi/hsl/transitdata/pulsarpubtransconnect/ArrivalHandler.java
@@ -1,6 +1,6 @@
 package fi.hsl.transitdata.pulsarpubtransconnect;
 
-import fi.hsl.transitdata.proto.PubtransTableProtos;
+import fi.hsl.common.transitdata.proto.PubtransTableProtos;
 import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.MessageBuilder;
 import org.apache.pulsar.client.api.Producer;
@@ -13,7 +13,7 @@ import java.util.Queue;
 
 public class ArrivalHandler extends PubtransTableHandler {
 
-    public ArrivalHandler(Producer producer) {
+    public ArrivalHandler(Producer<byte[]> producer) {
         super(producer);
     }
 
@@ -26,37 +26,40 @@ public class ArrivalHandler extends PubtransTableHandler {
 
         while (resultSet.next()) {
 
-            PubtransTableProtos.ROIArrival.Builder arrivalBuilder = PubtransTableProtos.ROIArrival.newBuilder();
+            PubtransTableProtos.ROIBase.Builder baseBuilder = PubtransTableProtos.ROIBase.newBuilder();
 
             //This is the LastModifiedUTCDateTime from the database. It will be used in the actual protobuf message and
             //the Pulsar message eventTime field
             long eventTime = resultSet.getTimestamp(16).getTime();
             if (eventTime > tempTimeStamp) tempTimeStamp = eventTime;
 
-            arrivalBuilder.setId(resultSet.getLong(1));
-            arrivalBuilder.setIsOnDatedVehicleJourneyId(resultSet.getLong(2));
-            if (resultSet.getBytes(3) != null) arrivalBuilder.setIsOnMonitoredVehicleJourneyId(resultSet.getLong(3));
-            arrivalBuilder.setJourneyPatternSequenceNumber(resultSet.getInt(4));
-            arrivalBuilder.setIsTimetabledAtJourneyPatternPointGid(resultSet.getLong(5));
-            arrivalBuilder.setVisitCountNumber(resultSet.getInt(6));
-            if (resultSet.getBytes(7) != null) arrivalBuilder.setIsTargetedAtJourneyPatternPointGid(resultSet.getLong(7));
-            if (resultSet.getBytes(8) != null) arrivalBuilder.setWasObservedAtJourneyPatternPointGid(resultSet.getLong(8));
-            if (resultSet.getBytes(9) != null) arrivalBuilder.setTimetabledLatestDateTime(resultSet.getString(9));
-            if (resultSet.getBytes(10) != null) arrivalBuilder.setTargetDateTime(resultSet.getString(10));
-            if (resultSet.getBytes(11) != null) arrivalBuilder.setEstimatedDateTime(resultSet.getString(11));
-            if (resultSet.getBytes(12) != null) arrivalBuilder.setObservedDateTime(resultSet.getString(12));
-            arrivalBuilder.setState(resultSet.getLong(13));
-            arrivalBuilder.setType(resultSet.getInt(14));
-            arrivalBuilder.setIsValidYesNo(resultSet.getBoolean(15));
-            arrivalBuilder.setLastModifiedUTCDateTime(eventTime);
+            baseBuilder.setId(resultSet.getLong(1));
+            baseBuilder.setIsOnDatedVehicleJourneyId(resultSet.getLong(2));
+            if (resultSet.getBytes(3) != null) baseBuilder.setIsOnMonitoredVehicleJourneyId(resultSet.getLong(3));
+            baseBuilder.setJourneyPatternSequenceNumber(resultSet.getInt(4));
+            baseBuilder.setIsTimetabledAtJourneyPatternPointGid(resultSet.getLong(5));
+            baseBuilder.setVisitCountNumber(resultSet.getInt(6));
+            if (resultSet.getBytes(7) != null) baseBuilder.setIsTargetedAtJourneyPatternPointGid(resultSet.getLong(7));
+            if (resultSet.getBytes(8) != null) baseBuilder.setWasObservedAtJourneyPatternPointGid(resultSet.getLong(8));
+            if (resultSet.getBytes(9) != null) baseBuilder.setTimetabledLatestDateTime(resultSet.getString(9));
+            if (resultSet.getBytes(10) != null) baseBuilder.setTargetDateTime(resultSet.getString(10));
+            if (resultSet.getBytes(11) != null) baseBuilder.setEstimatedDateTime(resultSet.getString(11));
+            if (resultSet.getBytes(12) != null) baseBuilder.setObservedDateTime(resultSet.getString(12));
+            baseBuilder.setState(resultSet.getLong(13));
+            baseBuilder.setType(resultSet.getInt(14));
+            baseBuilder.setIsValidYesNo(resultSet.getBoolean(15));
+            baseBuilder.setLastModifiedUTCDateTime(eventTime);
 
+            PubtransTableProtos.ROIBase base = baseBuilder.build();
+            PubtransTableProtos.ROIArrival.Builder arrivalBuilder = PubtransTableProtos.ROIArrival.newBuilder();
+            arrivalBuilder.setBase(base);
             PubtransTableProtos.ROIArrival arrival = arrivalBuilder.build();
 
             TypedMessageBuilder msgBuilder = producer.newMessage()
                     .key(resultSet.getString(2) + resultSet.getString(4))
                     .eventTime(eventTime)
                     .property("table-name", "roi-arrival")
-                    .property("dvj-id", String.valueOf(arrival.getIsOnDatedVehicleJourneyId()))
+                    .property("dvj-id", String.valueOf(base.getIsOnDatedVehicleJourneyId()))
                     .value(arrival.toByteArray());
 
             messageBuilderQueue.add(msgBuilder);
--- src/main/java/fi/hsl/transitdata/pulsarpubtransconnect/DepartureHandler.java
+++ src/main/java/fi/hsl/transitdata/pulsarpubtransconnect/DepartureHandler.java
@@ -1,6 +1,6 @@
 package fi.hsl.transitdata.pulsarpubtransconnect;
 
-import fi.hsl.transitdata.proto.PubtransTableProtos;
+import fi.hsl.common.transitdata.proto.PubtransTableProtos;
 import org.apache.pulsar.client.api.Message;
 import org.apache.pulsar.client.api.MessageBuilder;
 import org.apache.pulsar.client.api.Producer;
@@ -30,40 +30,44 @@ public class DepartureHandler extends PubtransTableHandler {
 
         while (resultSet.next()) {
 
-            PubtransTableProtos.ROIDeparture.Builder departureBuilder = PubtransTableProtos.ROIDeparture.newBuilder();
+            PubtransTableProtos.ROIBase.Builder baseBuilder = PubtransTableProtos.ROIBase.newBuilder();
 
             //This is the LastModifiedUTCDateTime from the database. It will be used in the actual protobuf message and
             //the Pulsar message eventTime field
             long eventTime = resultSet.getTimestamp(19).getTime();
             if (eventTime > tempTimeStamp) tempTimeStamp = eventTime;
 
-            departureBuilder.setId(resultSet.getLong(1));
-            departureBuilder.setIsOnDatedVehicleJourneyId(resultSet.getLong(2));
-            if (resultSet.getBytes(3) != null) departureBuilder.setIsOnMonitoredVehicleJourneyId(resultSet.getLong(3));
-            departureBuilder.setJourneyPatternSequenceNumber(resultSet.getInt(4));
-            departureBuilder.setIsTimetabledAtJourneyPatternPointGid(resultSet.getLong(5));
-            departureBuilder.setVisitCountNumber(resultSet.getInt(6));
-            if (resultSet.getBytes(7) != null) departureBuilder.setIsTargetedAtJourneyPatternPointGid(resultSet.getLong(7));
-            if (resultSet.getBytes(8) != null) departureBuilder.setWasObservedAtJourneyPatternPointGid(resultSet.getLong(8));
+            baseBuilder.setId(resultSet.getLong(1));
+            baseBuilder.setIsOnDatedVehicleJourneyId(resultSet.getLong(2));
+            if (resultSet.getBytes(3) != null) baseBuilder.setIsOnMonitoredVehicleJourneyId(resultSet.getLong(3));
+            baseBuilder.setJourneyPatternSequenceNumber(resultSet.getInt(4));
+            baseBuilder.setIsTimetabledAtJourneyPatternPointGid(resultSet.getLong(5));
+            baseBuilder.setVisitCountNumber(resultSet.getInt(6));
+            if (resultSet.getBytes(7) != null) baseBuilder.setIsTargetedAtJourneyPatternPointGid(resultSet.getLong(7));
+            if (resultSet.getBytes(8) != null) baseBuilder.setWasObservedAtJourneyPatternPointGid(resultSet.getLong(8));
+            if (resultSet.getBytes(12) != null) baseBuilder.setTimetabledLatestDateTime(resultSet.getString(12));
+            if (resultSet.getBytes(13) != null) baseBuilder.setTargetDateTime(resultSet.getString(13));
+            if (resultSet.getBytes(14) != null) baseBuilder.setEstimatedDateTime(resultSet.getString(14));
+            if (resultSet.getBytes(15) != null) baseBuilder.setObservedDateTime(resultSet.getString(15));
+            baseBuilder.setState(resultSet.getLong(16));
+            baseBuilder.setType(resultSet.getInt(17));
+            baseBuilder.setIsValidYesNo(resultSet.getBoolean(18));
+            baseBuilder.setLastModifiedUTCDateTime(eventTime);
+
+            PubtransTableProtos.ROIBase base = baseBuilder.build();
+
+            PubtransTableProtos.ROIDeparture.Builder departureBuilder = PubtransTableProtos.ROIDeparture.newBuilder();
+            departureBuilder.setBase(base);
             if (resultSet.getBytes(9) != null) departureBuilder.setHasDestinationDisplayId(resultSet.getLong(9));
             if (resultSet.getBytes(10) != null) departureBuilder.setHasDestinationStopAreaGid(resultSet.getLong(10));
             if (resultSet.getBytes(11) != null) departureBuilder.setHasServiceRequirementId(resultSet.getLong(11));
-            if (resultSet.getBytes(12) != null) departureBuilder.setTimetabledLatestDateTime(resultSet.getString(12));
-            if (resultSet.getBytes(13) != null) departureBuilder.setTargetDateTime(resultSet.getString(13));
-            if (resultSet.getBytes(14) != null) departureBuilder.setEstimatedDateTime(resultSet.getString(14));
-            if (resultSet.getBytes(15) != null) departureBuilder.setObservedDateTime(resultSet.getString(15));
-            departureBuilder.setState(resultSet.getLong(16));
-            departureBuilder.setType(resultSet.getInt(17));
-            departureBuilder.setIsValidYesNo(resultSet.getBoolean(18));
-            departureBuilder.setLastModifiedUTCDateTime(eventTime);
-
             PubtransTableProtos.ROIDeparture departure = departureBuilder.build();
 
             TypedMessageBuilder<byte[]> msgBuilder = producer.newMessage()
                     .key(resultSet.getString(2) + resultSet.getString(4))
                     .eventTime(eventTime)
                     .property("table-name", "roi-departure")
-                    .property("dvj-id", String.valueOf(departure.getIsOnDatedVehicleJourneyId()))
+                    .property("dvj-id", String.valueOf(base.getIsOnDatedVehicleJourneyId()))
                     .value(departure.toByteArray());
 
             messageBuilderQueue.add(msgBuilder);
